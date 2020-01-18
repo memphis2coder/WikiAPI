ca72b47fa8516b337e994413a82b830f56cb39b7
antonio kim <antoniokim@antonios-Air.fios-router.home>
2020-01-18 17:35:10 -0500

    complete api

--- app.js
+++ app.js
@@ -24,26 +24,97 @@ const articleSchema = {
     content: String
 }
 // in order to use our schema we need to convert our articleSchema to a MODEL
+// Article is the name of the model
 const Article = mongoose.model('Article', articleSchema);
 
 // getting data from mongodb from the collections title of articles
-app.get('/articles', function(req, res) {
-    Article.find(function(err, foundArticles) {
-        if (!err) {
-            res.send(foundArticles)
-        } else {
-            res.send(err)
-        }
-        
+app.route('/articles')
+    .get(function (req, res) {
+        Article.find(function(err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles)
+            } else {
+                res.send(err)
+            }
+        })
     })
-})
-// post new data to mongodb
-app.post('/articles', function(req,res) {
-    console.log(req.body.title)
-    console.log(req.body.content)
-})
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        })
+        
+        newArticle.save(function(err) {
+            if (!err){
+                res.send('Successfully added a new article')
+            } else {
+                res.send(err)
+            }
+        })
+    })
+    .delete(function (req, res) {
+        Article.deleteMany(function(err) {
+            if (!err) {
+                console.log('Successfully deleted article')
+            } else {
+                res.send(err)
+            }
+        })
+    })
 
-// s
+    // route parameters specified field
+app.route('/articles/:articleTitle')
+    .get(function(req,res) {
+        
+        Article.findOne({title: req.params.articleTitle}, function(err, foundArticle) {
+            if (foundArticle) {
+                res.send(foundArticle)
+            } else {
+                res.send("no article with that title was found.")
+            }
+        }) 
+    })
+    .put(function(req,res) {
+        Article.updateOne(
+            {title: req.params.articleTitle},
+            {title: req.body.title, content: req.body.content},
+            {overwrite: true},
+            function(err) {
+                if (!err) {
+                    res.send("success update article")
+                }
+            }
+        );
+    })
+    .patch(function(req, res) {
+        Article.update(
+            {title: req.params.articleTitle},
+            {$set: req.body},
+            function(err) {
+                if(!err) {
+                    console.log('success updated article')
+                } else {
+                    res.send(err);
+                }
+            }
+        )
+    })
+    .delete(function(req,res) {
+        Article.deleteOne(
+            {title: req.params.articleTitle},
+            function(err) {
+                if (!err) {
+                    console.log("success deleted one")
+                } else {
+                    console.log(err)
+                }
+            }
+        )
+    });
+
+
+
+// start server all port 3000
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
